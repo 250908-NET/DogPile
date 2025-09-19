02d163e6bcd57540b3fb5df096a127c410006dc3
Dean <deanscottg@gmail.com>
2025-09-19 16:45:21 -0400

    end of week1

--- Week1/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ Week1/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ac3e44b8411b74c18923f60778a05e28a12dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc628b74406d6ee31afd33eee5e3e4bb00590e4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
